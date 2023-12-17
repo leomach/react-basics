39a24260a51417bc2d5adbad224388af063a3fec
Leandro Machado <leandrovictoram@gmail.com>
2023-12-17 11:56:43 -0300

    build: organização e componentização funcional

---
+++ src/templates/Home/index.jsx
@@ -0,0 +1,32 @@
+import "./styles.css";
+import { Component } from "react";
+
+import { Posts } from "../../components/Posts";
+import { loadPosts } from "../../utils/load-posts";
+
+class Home extends Component {
+  state = {
+    posts: [],
+  };
+
+  async componentDidMount() {
+    await this.loadPosts();
+  }
+
+  loadPosts = async () => {
+    const postsAndPhotos = await loadPosts();
+    this.setState({ posts: postsAndPhotos });
+  };
+
+  render() {
+    const { posts } = this.state;
+
+    return (
+      <section className="container">
+        <Posts posts={posts} />
+      </section>
+    );
+  }
+}
+
+export default Home;
